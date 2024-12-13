7e1650516ec36c7012b1894c7786af13989dd5b7
Tran Nam Dung <trannamdung7@gmail.com>
2024-12-13 21:10:33 +0700

    Init Success

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -9,25 +9,27 @@ import vn.hoidanit.laptopshop.service.UserServices;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
-    private UserServices userServices;
 
-    public UserController(UserServices userServices) {
-        this.userServices = userServices;
-    }
-
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userServices.handleHello();
+        return "eric.html";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// //DI: Dependency Injection
+// private UserServices userServices;
+
+// public UserController(UserServices userServices) {
+// this.userServices = userServices;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userServices.handleHello();
+// }
+// }
